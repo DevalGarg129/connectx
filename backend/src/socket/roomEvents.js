c6dc49d7f521bd10255431bac6518824820ef4f5
devalgarg129 <devalgarg978@gmail.com>
2026-07-16 15:09:52 +0530

    implementing room Events

--- backend/src/socket/roomEvents.js
+++ backend/src/socket/roomEvents.js
@@ -1,32 +1,74 @@
-export const SOCKET_EVENTS = {
-    //Connection
-    CONNECTION: "connection",
-    DISCONNECT: "disconnect",
+import roomManager from "./roomManager.js";
+import { SOCKET_EVENTS } from "./socketEvents.js";
+import socketAsyncHandler from "../socket/socketAsyncHandler.js";
 
-    //Room
-    JOIN_ROOM: "join-room",
-    LEAVE_ROOM: "leave-room",
-    USER_JOINED: "user-joined",
-    USER_LEFT: "user-left",
-    ROOM_USERS: "room-users",
-    ROOM_ENDED: "room-ended",
+//function for handloing duplicates
+const removeParticipant = (socket) => {
+    const roomId = socket.roomId;
+    if(!roomId) return;
 
-    //Web RTC
-    OFFER: "offer",
-    ANSWER: "answer",
-    ICE_CANDIDATE: "ice-candidate",
-
-    //Media
-    MUTE: "mute",
-    UNMUTE: "unmute",
-    VIDEO_ON: "video-on",
-    VIDEO_OFF: "video-off",
-    SCREEN_SHARE_START: "screen-share-start",
-    SCREEN_SHARE_STOP: "screen-share-stop",
-    RAISE_HAND: "raise-hand",
-
-    //chat message
-    CHAT_MESSAGE: "chat-message",
-    TYPING: "typing",
-    STOP_TYPING: "stop_typing"
+    roomManager.removeUser(roomId, socket.id);
+    socket.leave(roomId);
+    socket.to(roomId).emit(
+        SOCKET_EVENTS.USER_LEFT,{
+            socketId: socket.id,
+            userId: socket.userId
+        }
+    );
 };
+
+const registerRoomEvents = (io, socket) => {
+    //join room
+    socket.on(
+        SOCKET_EVENTS.JOIN_ROOM,
+        socketAsyncHandler(async ({ roomId, userId, username }) => {
+            if(!roomId || !userId || !username){
+                return socket.emit("error", {
+                    message: "Invalid room join request."
+                });
+            }
+            socket.join(roomId);
+
+            socket.roomId = roomId;
+            socket.userId = userId;
+            socket.username = username;
+
+            roomManager.addUser(roomId, socket, {
+                userId,
+                username
+            });
+
+            const participants = roomManager.getUsers(roomId);
+
+            socket.emit(SOCKET_EVENTS.ROOM_USERS, participants);
+
+            //notify everyone except sender 
+            socket.to(roomId).emit(
+                SOCKET_EVENTS.USER_JOINED,{
+                    socketId: socket.id,
+                    userId,
+                    username
+                }
+            );
+            console.log(`${username} joined room ${roomId}`);
+        })
+    );
+
+    //leave room
+    socket.on(
+        SOCKET_EVENTS.LEAVE_ROOM,
+        socketAsyncHandler(async () => {
+            removeParticipant(socket);
+        })
+    );
+
+    //disconnect
+    socket.on(
+        SOCKET_EVENTS.DISCONNECT,
+        socketAsyncHandler(async () => {
+            removeParticipant(socket);
+        })
+    );
+};
+
+export default registerRoomEvents;
---
+++ backend/src/socket/socketEvents.js
@@ -0,0 +1,32 @@
+export const SOCKET_EVENTS = {
+    //Connection
+    CONNECTION: "connection",
+    DISCONNECT: "disconnect",
+
+    //Room
+    JOIN_ROOM: "join-room",
+    LEAVE_ROOM: "leave-room",
+    USER_JOINED: "user-joined",
+    USER_LEFT: "user-left",
+    ROOM_USERS: "room-users",
+    ROOM_ENDED: "room-ended",
+
+    //Web RTC
+    OFFER: "offer",
+    ANSWER: "answer",
+    ICE_CANDIDATE: "ice-candidate",
+
+    //Media
+    MUTE: "mute",
+    UNMUTE: "unmute",
+    VIDEO_ON: "video-on",
+    VIDEO_OFF: "video-off",
+    SCREEN_SHARE_START: "screen-share-start",
+    SCREEN_SHARE_STOP: "screen-share-stop",
+    RAISE_HAND: "raise-hand",
+
+    //chat message
+    CHAT_MESSAGE: "chat-message",
+    TYPING: "typing",
+    STOP_TYPING: "stop_typing"
+};
